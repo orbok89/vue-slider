be369c6d1cf934b06fe1e789a5dc50d01512bfa0
BigFelk <fedferrara89@gmail.com>
2021-09-08 16:18:04 +0200

    sroll finito

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 var app = new Vue(
     {
         el: '#root',
+
         data:{
-            messaggio: 'Questo è un testo di prova'
+            contatore: 0,
+            attivo: 'blu',
+            immagini:[
+               "img/image1.jpg",
+               "img/image2.jpg", 
+               "img/image3.jpg", 
+               "img/image4.jpg"
+            ]
+        },
+        methods:{
+            destra: function(){
+                this.contatore= this.contatore+1;
+                if(this.contatore == this.immagini.length){
+                    this.contatore=0;
+                }
+            },
+            sinistra: function(){
+                this.contatore= (this.contatore - 1);
+                if(this.contatore < 0){
+                    this.contatore = (this.immagini.length -1);
+                }
+            }
         }
     }
 )
